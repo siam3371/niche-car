4fa053007d1d6875fcce3e8a1ac86dfef872b2e8
siam3371 <siamhossain3371@gmail.com>
2021-11-19 12:39:45 +0600

    first commit

--- src/Pages/Dashboard/ManageProducts/ManageProduct/ManageProduct.js
+++ src/Pages/Dashboard/ManageProducts/ManageProduct/ManageProduct.js
@@ -1,43 +1,43 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
-const ManageProduct = ({product}) => {
-      const {name, describe, img, price, _id, setProducts} = product
-       const handleDeleteProduct =()=>{
-        const proceed = window.confirm('Are You surely remove this Purches Order');
-        if(proceed){
-          fetch(`http://localhost:5000/products/${_id}`, {
-              method:'DELETE',
-          })
-          .then(res=>res.json())
-          .then(data=>{
-            console.log(data) 
-            if(data.deletedCount > 0){
-              alert('deleted successfully');
-               const remainigOrder = data.filter(order => order._id === _id);
-              setProducts(remainigOrder);
-              console.log(remainigOrder)
-            }
-          });
-        }
-          // console.log(products)
-      } 
-    return (
-        <>
-            <div className="col-lg-4 col-md-6 col-12">
-  {/* services item showing */}
-<div style={{
-      height:"630px"
-}} className="shadow-lg p-2 mb-5 bg-body rounded p-5">
-<img className="w-100" src={img} alt="" />
-<h3>{name}</h3>
-<p>{describe}</p> 
-<p>price:${price}</p>
- {/* button click */}  
- <button className="btn btn-primary w-50" onClick={handleDeleteProduct }>Delete </button>
- </div> 
-  </div>
-        </ >
-    );
-};
+// const ManageProduct = ({product}) => {
+//       const {name, describe, img, price, _id, setProducts} = product
+//        const handleDeleteProduct =()=>{
+//         const proceed = window.confirm('Are You surely remove this Purches Order');
+//         if(proceed){
+//           fetch(`http://localhost:5000/products/${_id}`, {
+//               method:'DELETE',
+//           })
+//           .then(res=>res.json())
+//           .then(data=>{
+//             console.log(data) 
+//             if(data.deletedCount > 0){
+//               alert('deleted successfully');
+//                const remainigOrder = data.filter(order => order._id === _id);
+//               setProducts(remainigOrder);
+//               console.log(remainigOrder)
+//             }
+//           });
+//         }
+//           // console.log(products)
+//       } 
+//     return (
+//         <>
+//             <div className="col-lg-4 col-md-6 col-12">
+//   {/* services item showing */}
+// <div style={{
+//       height:"630px"
+// }} className="shadow-lg p-2 mb-5 bg-body rounded p-5">
+// <img className="w-100" src={img} alt="" />
+// <h3>{name}</h3>
+// <p>{describe}</p> 
+// <p>price:${price}</p>
+//  {/* button click */}  
+//  <button className="btn btn-primary w-50" onClick={handleDeleteProduct }>Delete </button>
+//  </div> 
+//   </div>
+//         </ >
+//     );
+// };
 
-export default ManageProduct;
+// export default ManageProduct;
